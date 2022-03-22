7b2b5fd0dc0667f6d65e319033a599b3ff2f74c9
Mirsario <me@mirsar.io>
2022-03-23 00:21:55 +0300

    Restored hastily removed code.

--- Src/IPL.cs
+++ Src/IPL.cs
@@ -6,5 +6,15 @@
 	public static partial class IPL
 	{
 		public const string Library = "phonon.dll";
-    }
+
+		public partial struct Vector3
+		{
+			public Vector3(float x, float y, float z)
+			{
+				this.x = x;
+				this.y = y;
+				this.z = z;
+			}
+		}
+	}
 }
